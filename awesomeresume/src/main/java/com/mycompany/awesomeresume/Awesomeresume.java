67d196814ce9ff5bc244bf4cbfb5a4f39f5b7563
Hacibaba <aliyevhacibaba@gmail.com>
2024-08-19 21:03:25 +0400

    yeni awesomeresume proyekti yaradildi

---
+++ awesomeresume/src/main/java/com/mycompany/awesomeresume/Awesomeresume.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.awesomeresume;
+
+/**
+ *
+ * @author User
+ */
+public class Awesomeresume {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
